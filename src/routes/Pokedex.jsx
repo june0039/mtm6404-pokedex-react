1a5575a7345c477e2c283c7c599ff48fc1c12573
Julie <june0039@algonquinlive.com>
2022-11-16 12:49:08 -0500

    useEffect

--- src/routes/Pokedex.jsx
+++ src/routes/Pokedex.jsx
@@ -1,4 +1,11 @@
+import { useState, useEffect } from 'react'
+import Row from '../components/Row'
+import Col from '../components/Col'
+import PokemonCard from '../components/PokemonCard'
+
 function Pokedex() {
+
+
 
     return (
         <h1>Pokedex</h1>
