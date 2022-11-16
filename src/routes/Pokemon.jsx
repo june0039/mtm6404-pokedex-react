d397fa9cf82ead3690efbaba06e4b67632a9e377
Julie <june0039@algonquinlive.com>
2022-11-16 13:16:09 -0500

    pokemon

--- src/routes/Pokemon.jsx
+++ src/routes/Pokemon.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Row from '../components/Row'
+import Col from '../components/Col'
+
+
 function Pokemon() {
+    const params = useParams()
+    const [pokemon, setPokemon] = useState({
+        name: '',
+        id: '',
+        image: '',
+        abilities: []
+    })
+
+    useEffect(() => {
+
+        fetch('https://pokeapi.co/api/v2/pokemon/' + params.name)
+            .then(response => response.json())
+            .then(json => { setPokedex(json.results) })
+    })
 
     return (
-        <h1>Pokemon</h1>
+        <h1>{params.name}</h1>
     )
 }
 
