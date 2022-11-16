6bf1e3c7793cf41fbada3e062f6571e04dca5889
Julie <june0039@algonquinlive.com>
2022-11-16 12:42:54 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,25 @@ function App() {
 
   return (
     <>
+
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink to="./routes/Pokedex.jsx" className={isActive => isActive ? 'nav-link active' : 'nav-link'}>Pokedex</NavLink >
+              </li>
+              <li class="nav-item">
+                <NavLink to="./routes/Items.jsx" className={isActive => isActive ? 'nav-link active' : 'nav-link'}>Items</NavLink >
+              </li>
+              <li class="nav-item">
+                <NavLink to="./routes/Pokemon.jsx" className={isActive => isActive ? 'nav-link active' : 'nav-link'}>Pokemon</NavLink >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
       <div className="container">
         <Outlet />
 
--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <routerProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 
 )
